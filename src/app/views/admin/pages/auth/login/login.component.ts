89e367005547630be5164e39c3b574f06ace73c7
Ornice <ornihouss1@gmail.com>
2025-09-20 21:46:58 +0100

    fix data

--- src/app/views/admin/pages/auth/login/login.component.ts
+++ src/app/views/admin/pages/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/core/services/auth.service';
@@ -11,25 +12,34 @@ import { LocalStorageService } from 'src/app/core/utils/local-stoarge-service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+authForm: FormGroup;
+  isLoading = false;
   loading=false
  
   constructor(
     private authService:AuthService,
     private lsService:LocalStorageService,
     private router: Router,
+    private fb: FormBuilder,
     private toastr: ToastrService
-    ) { }
+    ) { 
+      this.authForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+    }
 
   ngOnInit(): void {
   }
 
 
-  login(value:any){
+  login(){
     //this.router.navigate(['/admin/dashboard'])
+    if (this.authForm?.invalid) return;
+    const { email, password } = this.authForm?.value;
 
     this.loading=true
-    this.authService.login(value).subscribe((res:any)=>{
+    this.authService.login({ email, password }).subscribe((res:any)=>{
       this.loading=false
 
       this.lsService.set(GlobalName.tokenName,res.access_token)
@@ -44,7 +54,7 @@ export class LoginComponent implements OnInit {
     },
     (err:any)=>{
       this.loading=false
-      this.toastr.error('Connexion échouée', 'Connexion');
+      this.toastr.error(err.error?.message, 'Connexion');
 
     });
   }
