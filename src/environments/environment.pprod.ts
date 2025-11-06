1e88de9f622673e9fd204a64b3d05d08b6a05511
Ornice <ornihouss1@gmail.com>
2025-11-06 19:45:04 +0100

    fix data

--- src/environments/environment.pprod.ts
+++ src/environments/environment.pprod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   isMockEnabled: false, // You have to switch this, when your real back-end is done
   authTokenKey: 'authce9d77b308c149d5992a80073637e4d5',
-  API_SCHEME: 'https',
+  API_SCHEME: 'http',
   API_DOMAIN: 'test2plateforme.social.gouv.bj/backend/api',
   API_FILE: 'test2plateforme.social.gouv.bj/backend',
   API_VERSION: 'v1',
