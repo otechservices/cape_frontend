52374aca5c1e55847ceddf57c25abe83ed220654
Ornice <ornihouss1@gmail.com>
2025-11-06 19:13:27 +0100

    fix data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,10 +3,10 @@ export const environment = {
   isMockEnabled: false, // You have to switch this, when your real back-end is done
   authTokenKey: 'authce9d77b308c149d5992a80073637e4d5',
   API_SCHEME: 'https',
-  API_DOMAIN: 'apicape.hebergeappli.bj/api',
-  API_FILE: 'apicape.hebergeappli.bj',
+  API_DOMAIN: 'cape.social.gouv.bj/backend/api',
+  API_FILE: 'cape.social.gouv.bj/backend',
   API_VERSION: 'v1',
   DIST_DIR: 'dist',
   HOME_URI: 'accueil',
-  ADMIN_URL:'https://cape.hebergeappli.bj/admin/auth/login'
+  ADMIN_URL:'https://capemanager.social.gouv.bj'
 };
