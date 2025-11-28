eb9f94ef0d24c213766f9920ec028e00c0d2ae37
Ornice <ornihouss1@gmail.com>
2025-11-28 14:10:05 +0100

    fix police

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,25 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-   prefix: 'tw-',
+  prefix: 'tw-',
   content: [
     "./src/**/*.{html,ts}",
     "./node_modules/primeng/**/*.js"
   ],
   darkMode: 'class',
-  theme: {
-    extend: {
-    },
-  },
-  plugins: [
-     require('@tailwindcss/forms'),
-  ],
 
   theme: {
-    fontFamily: {
-      sans: [
-        'system-ui',
-        '-apple-system',
-        '"Segoe UI"',
-        'Roboto',
-        '"Helvetica Neue"',
-        '"Noto Sans"',
-        '"Liberation Sans"',
-        'Arial',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-        '"Noto Color Emoji"',
-      ]
-    }
-  }
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'ui-sans-serif', 'system-ui'],
+        title: ['Montserrat', 'ui-sans-serif', 'system-ui'],
+      },
+    },
+  },
+
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
 
 export default config
